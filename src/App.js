ec2679903e03132287e316cb10bf10c1c5c82bd2
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-03-03 17:39:46 +0100

    test cases written

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TDD App </h1>
+      <h3>This App is developed by using TDD approach</h3>
+      <p>Build CI/CD pipeline by using TravisCI and deployed on Surge</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/App/i);
+  expect(linkElement).toBeInTheDocument();
+}); */
+
+
+
+
+test('find deployed world ', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Deployed/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('find deployed world ', () => {
+  render(<App />);
+  const linkElement1 = screen.getByText(/Surge/i);
+  expect(linkElement1).toBeInTheDocument();
+});
+
